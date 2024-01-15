c4fed3905a9a8e016dea00a35507e77e160552ff
Максим <oneskillonesoft@gmail.com>
2024-01-15 08:27:58 +0300

    Решение задачи 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,41 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Никита", "Пермяков", (byte) 30);
+        System.out.println("User с именем – Никита добавлен в базу данных");
+
+        userService.saveUser("Алена", "Ютаева", (byte) 25);
+        System.out.println("User с именем – Алена добавлен в базу данных");
+
+        userService.saveUser("Владислав", "Макаров", (byte) 22);
+        System.out.println("User с именем – Владислав добавлен в базу данных");
+
+        userService.saveUser("Петр", "Кунов", (byte) 28);
+        System.out.println("User с именем – Петр добавлен в базу данных");
+
+        List<User> users = userService.getAllUsers();
+        if (users != null) {
+            for (User user : users) {
+                System.out.println(user);
+            }
+        } else {
+            System.out.println("Не удалось получить список пользователей.");
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
+
