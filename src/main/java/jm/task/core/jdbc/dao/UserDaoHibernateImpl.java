3115a0a9d86f72804b1c56a90333d48bf5d29db0
Максим <oneskillonesoft@gmail.com>
2024-01-19 10:44:12 +0300

    решение задачи 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.QueryException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -13,30 +20,99 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+                String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "Id SERIAL PRIMARY KEY NOT NULL," +
+                "Name CHARACTER VARYING(30)," +
+                "LastName CHARACTER VARYING(30)," +
+                "Age INTEGER);";
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            NativeQuery<User>query = session.createNativeQuery(sql);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
 
+        String sql = "DROP TABLE IF EXISTS Users";
+
+        Sql(sql);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        String sql = "INSERT INTO users(name, lastname, age) VALUES ('" + name + "', '" + lastName + "', " + age + ")";
+
+        Sql(sql);
+    }
+
+    private void Sql(String sql) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            NativeQuery<User> query = session.createNativeQuery(sql);
+            query.executeUpdate();
+            transaction.commit();
+        }catch (QueryException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
 
+        String sql = "DELETE FROM users WHERE id = " + id;
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            NativeQuery<User>query = session.createNativeQuery(sql);
+            session.createSQLQuery("DELETE FROM users WHERE id = " + id);
+            query.executeUpdate();
+            transaction.commit();
+            if (id > 0) {
+                System.out.println("User с id " + id + " удалён из базы данных");
+            } else {
+                System.out.println("Ошибка! id должен быть больше нуля.");
+            }
+        }catch (QueryException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User>users = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            users = session.createQuery("FROM User").getResultList();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
 
+        String sql = "DELETE FROM Users";
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            NativeQuery query = session.createSQLQuery(sql);
+            query.executeUpdate();
+            transaction.commit();
+        }catch (QueryException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,26 +1,65 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    private static final String URL = "jdbc:postgresql://localhost/postgres";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "root";
 
-    public static Connection getConnection() {
+    public static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String userName = "postgres";
+    public static final String password = "root";
 
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Соединениние успешно...");
-        } catch (SQLException e) {
-            System.out.println("Соединениние не успешно...");
-            throw new RuntimeException(e);
 
+    private static final String DRIVER = "org.postgresql.Driver";
+
+    private static SessionFactory sessionFactory;
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, userName, password);
+    }
+
+    public static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.url", url);
+        properties.setProperty("hibernate.connection.username", userName);
+        properties.setProperty("hibernate.connection.password", password);
+        properties.setProperty("hibernate.connection.driver_class", DRIVER);
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.current_session_context_class", "thread");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.connection.characterEncoding", "utf8");
+        properties.setProperty("hibernate.current_session_context_class", "thread");
+
+
+//    <property name = "hibernate.connection.characterEncoding" > utf8 </property >
+//    <property name = "hibernate.current_session_context_class" > thread </property >
+
+        return properties;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.setProperties(getProperties());
+            configuration.addAnnotatedClass(User.class);
+
+            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
-
-        return conn;
+        return sessionFactory;
     }
 }
+
